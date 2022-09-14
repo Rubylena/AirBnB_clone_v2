744494c92b328d0eee1a8baaf8627d733be73404
Grace <graceffiong@gmail.com>
2022-09-14 12:51:40 -0700

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,12 +7,12 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
 	IF NOT EXISTS 'hbnb_dev'@'localhost'
-	IDENTIFIED BY '{Hbnb_dev_pwd@1}';
+	IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.*
 	TO 'hbnb_dev'@'localhost'
-	IDENTIFIED BY '{Hbnb_dev_pwd@1}';
+	WITH GRANT OPTION;
 GRANT SELECT
 	ON `performance_schema`.*
 	TO 'hbnb_dev'@'localhost'
-	IDENTIFIED BY '{Hbnb_dev_pwd@1}';
+	WITH GRANT OPTION;
 FLUSH PRIVILEGES;
