2fbb35a8c34d139c5c82533339437872ff812201
Grace <graceffiong@gmail.com>
2022-10-05 14:25:03 +0000

    if statement errors

--- 0-setup_web_static_static.sh
+++ 0-setup_web_static_static.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #set up your web servers for the deployment of web_static
 
-if [ 'dpkg -l | grep -c nginx' eq 0 ]
+command=var$(dpkg -l | grep -c nginx)
+if [ "$command" -eq 0 ]
 then
 	apt-get update -y
 	apt-get install -y nginx
@@ -10,7 +11,7 @@ fi
 mkdir -p /data/web_static/releases/test/index.html
 mkdir -p /data/web_static/shared/
 ln -s -f /data/web_static/releases/test/ /data/web_static/current
-chown -R $USER:$USER /data/
+chown -R "$USER":"$USER" /data/
 echo 'Hello there!!!' > /data/web_static/releases/test/index.html
 echo "server {
     listen 80;
