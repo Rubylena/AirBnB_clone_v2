b216ddf313dee5dee8de99abd71ed7b8c8748948
Grace Effiong <graceffiong@gmail.com>
2022-10-10 13:47:54 +0000

    change the relationship arrow position

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,93 +1,86 @@
-#Time to practice configuring your server with Puppet! Just as 
-#you did before, we’d like you to install and configure an Nginx
-#server using Puppet instead of Bash.
-#create web_static folder
-
-$nginx_congf = "server {
+# Nginx configuration file
+$nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
+    add_header X-Served-By ${hostname};
     root   /var/www/html;
-    index  index.html index.htm index.nginx-debian.html;
-    server_name _;
-    
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-	index index.html index.htm;
-	autoindex off;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://graceeffiong.tech;
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlw\u4? permanent;
     }
     error_page 404 /404.html;
     location /404 {
-      root /var/www/error;
+      root /var/www/html;
       internal;
     }
 }"
 
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt',
-} ->
+  provider => 'apt'
+}
 
-file { '/data':
+-> file { '/data':
   ensure  => 'directory'
-} ->
+}
 
-file { '/data/web_static':
-  ensure  => 'directory'
-} ->
+-> file { '/data/web_static':
+  ensure => 'directory'
+}
 
-file { '/data/web_static/releases':
-  ensure  => 'directory'
-} ->
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
+}
 
-file { '/data/web_static/releases/test':
-  ensure  => 'directory'
-} ->
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
 
-file{ '/data/web_static/shared':
-  ensure  => 'directory'
-} ->
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
 
-file { '/data/web_static/releases/test/index.html':
+-> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "School puppet content\n"
-} ->
+  content => "Holberton School Puppet\n"
+}
 
-file { '/data/web_static/current':
-  ensure   => 'link',
-  target   => '/data/web_static/releases/test'
-} ->
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+}
 
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path    => '/usr/bin/:/usr/local/bin/:/bin/'
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html':
+-> file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
-file { '/var/www/html/index.html':
+-> file { '/var/www/html/index.html':
   ensure  => 'present',
   content => "Holberton School Nginx\n"
-} ->
+}
 
-file { '/var/www/html/404.html':
+-> file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-} ->
+}
 
-file { '/etc/nginx/sites-available/default':
+-> file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => '$nginx_congf
-} ->
+  content => $nginx_conf
+}
 
-exec { 'service ginx restart':
-  path    => '/etc/init.d/nginx'
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
