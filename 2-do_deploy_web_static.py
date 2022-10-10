dd597203769dd649acbebe76d494e2c0b80b54d4
Grace Effiong <graceffiong@gmail.com>
2022-10-10 09:16:34 +0000

    curl working

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """create fabfile"""
 from fabric.api import put, run, env
+from fabric.contrib import files
 import os
 
-env.user = 'ubuntu'
 env.hosts = ['3.238.181.140', '3.236.65.72']
 
 
@@ -18,13 +18,13 @@ def do_deploy(archive_path):
     dest = data_path + file_name
 
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, '/tmp')
         run('sudo mkdir -p {}'.format(dest))
         run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(file_name, dest))
         run('sudo rm -f /tmp/{}.tgz'.format(file_name))
         run('sudo mv {}/web_static/* {}/'.format(dest, dest))
         run('sudo rm -rf {}/web_static/'.format(dest))
-        run('sudo rm -rf /data/web_static/current/')
+        run('sudo rm -rf /data/web_static/current')
         run('sudo ln -s {} /data/web_static/current'.format(dest))
         return True
     except RuntimeError:
