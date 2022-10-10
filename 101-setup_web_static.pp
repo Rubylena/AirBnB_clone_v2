3d54f19f1952fa15ee2314a12b42fea8b42b5a31
Grace Effiong <graceffiong@gmail.com>
2022-10-10 11:56:13 +0000

    puppet file for task 0

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,88 @@
+#Time to practice configuring your server with Puppet! Just as 
+#you did before, we’d like you to install and configure an Nginx
+#server using Puppet instead of Bash.
+#create web_static folder
+
+$nginx_congf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm index.nginx-debian.html;
+    server_name _;
+    
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+	index index.html index.htm;
+	autoindex off;
+    }
+    location /redirect_me {
+        return 301 http://graceeffiong.tech;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/error;
+      internal;
+    }
+}"
+
+# package { 'nginx':
+#  ensure   => 'present',
+#  provider => 'apt',
+# } ->
+
+# file { '/data':
+#  ensure   => 'directory'
+#} ->
+
+#file { '/data/web_static':
+#  ensure   => 'directory'
+#} ->
+
+#file { '/data/web_static/releases':
+#  ensure   => 'directory'
+#} ->
+
+#file { '/data/web_static/releases/test':
+#  ensure   => 'directory'
+#} ->
+
+#file{ '/data/web_static/shared':
+#  ensure   => 'directory'
+#} ->
+
+#file { '/data/web_static/releases/test/index.html':
+#  ensure   => 'present',
+#  content  => "School puppet content\n"
+#} ->
+
+#file { '/data/web_static/current':
+#  ensure   => 'link',
+#  target   => '/data/web_static/releases/test
+#} ->
+
+#exec { 'chown -R ubuntu:ubuntu /data/':
+#  path     => '/usr/bin/:/usr/local/bin/:/bin/'
+#}
+
+#file { '/etc/nginx/sites-available/default':
+#  ensure   => 'present',
+#  content  => '$nginx_congf
+#} ->
+
+#exec { 'nginx restart':
+#  path     => '/etc/init.d/'
+#}
+ 
+exec { 'task 0':
+  command  => 'apt-get update -y;
+  apt-get install nginx -y;
+  mkdir -p /data/web_static/releases/test/;
+  mkdir -p /data/web_static/shared/;
+  ln -sf /data/web_static/releases/test/ /data/web_static/current;
+  echo 'Hello there' > /data/web_static/releases/test/index.html;
+  sudo chown -R ubuntu:ubuntu /data/;
+  echo $nginx_congf > /etc/nginx/sites-available/default;
+  service nginx restart;',
+  provider => shell,
+}
