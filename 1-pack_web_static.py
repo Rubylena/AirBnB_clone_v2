2594f6285c8077d4f7ed73bbeacc0c5bc16e94fa
Grace Effiong <graceffiong@gmail.com>
2022-10-10 05:55:59 +0000

    tar file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,12 +5,14 @@ import time
 
 
 def do_pack():
-    """script that generates a .tgz archive from the contents of the web_static"""
+    """script that generates a .tgz archive from
+    the contents of the web_static"""
+
     Tformat = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
               format(Tformat))
         return ("versions/web_static_{:s}.tgz".format(Tformat))
-    except:
+    except RuntimeError:
         return None
