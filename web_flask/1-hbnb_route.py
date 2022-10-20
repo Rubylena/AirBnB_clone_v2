ba8695747a96af0eee7c6ab99ad83711fdf17a2a
Ruby <graceffiong@gmail.com>
2022-10-20 21:06:01 +0100

    pycodestyle and rename

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,10 +10,12 @@ def hello():
     """ hello function content """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """hbnh content"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
