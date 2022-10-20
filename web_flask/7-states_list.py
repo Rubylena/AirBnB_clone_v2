8849c1a82894c5a1678f80956c122904eeb2beb3
Ruby <graceffiong@gmail.com>
2022-10-20 23:26:20 +0100

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""import flask"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def fetchStates():
+    states = storage.all("States")
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def removeSession():
+    """ teardown current sql session"""
+    return storage.close
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
