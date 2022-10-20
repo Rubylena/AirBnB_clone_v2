e4ebb99c551fcc7dafdc369e3b2d813ef1601aaf
Ruby <graceffiong@gmail.com>
2022-10-20 12:42:33 +0100

    update port

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+
 """ import flask """
 from flask import Flask
 
@@ -12,4 +12,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
